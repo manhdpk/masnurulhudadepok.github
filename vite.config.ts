ba47c60a640ce6e027bf269bdd5fc95de0169914
manhdpk <arfanmaulana@madrasah.kemenag.go.id>
2025-08-14 01:17:11 +0700

    ganti wa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/website.github.io/", // sesuai nama repo
+  base: "",
   server: {
     host: "::",
     port: 8080,
