edd692d19f6cfaa9c52693f54e26c01c0ea2701e
manhdpk <arfanmaulana@madrasah.kemenag.go.id>
2025-08-15 08:36:23 +0700

    update berita

--- src/data/news.ts
+++ src/data/news.ts
@@ -3,19 +3,6 @@ import img2 from "@/assets/berita/pramuka.jpg";
 
 export const news = [
   {
-    id: 2,
-    title:
-      "Juara Program LEVEL UP AI, Dorong Kreativitas dan Literasi AI Generasi Muda",
-    excerpt:
-      "Selamat kami ucapkan kepada para juara Program LEVEL UP AI yang telah berhasil menunjukkan prestasi gemilang dalam ajang yang bertujuan menumbuhkan kreativitas dan literasi kecerdasan buatan (AI) di kalangan generasi muda.",
-    image: img1,
-    content: `
-      Program ini terselenggara berkat kolaborasi antara Vox Digital Kreatif dan Yayasan Keluarga Alumni Paskibra 99, yang berkomitmen menghadirkan wadah pembelajaran serta pengembangan kemampuan teknologi AI bagi para peserta. Inisiatif ini menjadi langkah penting dalam mempersiapkan generasi muda untuk menghadapi tantangan di era digital.
-Kegiatan ini tidak hanya meningkatkan kemampuan teknis para peserta, tetapi juga memperkuat semangat inovasi dan kreativitas dalam pengembangan solusi berbasis AI yang bermanfaat bagi masyarakat luas.
-Dengan adanya program seperti LEVEL UP AI, diharapkan semakin banyak generasi muda yang terinspirasi dan mampu mengembangkan potensinya dalam bidang teknologi, sehingga dapat berkontribusi pada kemajuan bangsa dan negara.
-    `,
-  },
-  {
     id: 1,
     title: "MA Nurul Huda Gelar Upacara Peringatan Hari Pramuka ke-64",
     excerpt:
@@ -34,6 +21,19 @@ Upacara dimulai tepat pukul 07.00 WIB dengan susunan acara:
 Selain sebagai peringatan, kegiatan ini menjadi momen untuk mempererat persaudaraan antaranggota Pramuka dan memperkuat karakter peserta didik.
 “Semoga semangat Hari Pramuka ini menjadi pendorong bagi kita untuk terus berkarya dan mengabdi bagi bangsa, agama, dan negara,” ujar Kepala Madrasah.
 MA Nurul Huda berharap semangat yang ditanamkan melalui kegiatan ini dapat membentuk generasi muda yang tangguh, berkarakter, dan siap menghadapi tantangan zaman.
+    `,
+  },
+  {
+    id: 2,
+    title:
+      "Juara Program LEVEL UP AI, Dorong Kreativitas dan Literasi AI Generasi Muda",
+    excerpt:
+      "Selamat kami ucapkan kepada para juara Program LEVEL UP AI yang telah berhasil menunjukkan prestasi gemilang dalam ajang yang bertujuan menumbuhkan kreativitas dan literasi kecerdasan buatan (AI) di kalangan generasi muda.",
+    image: img1,
+    content: `
+      Program ini terselenggara berkat kolaborasi antara Vox Digital Kreatif dan Yayasan Keluarga Alumni Paskibra 99, yang berkomitmen menghadirkan wadah pembelajaran serta pengembangan kemampuan teknologi AI bagi para peserta. Inisiatif ini menjadi langkah penting dalam mempersiapkan generasi muda untuk menghadapi tantangan di era digital.
+Kegiatan ini tidak hanya meningkatkan kemampuan teknis para peserta, tetapi juga memperkuat semangat inovasi dan kreativitas dalam pengembangan solusi berbasis AI yang bermanfaat bagi masyarakat luas.
+Dengan adanya program seperti LEVEL UP AI, diharapkan semakin banyak generasi muda yang terinspirasi dan mampu mengembangkan potensinya dalam bidang teknologi, sehingga dapat berkontribusi pada kemajuan bangsa dan negara.
     `,
   },
 ];
